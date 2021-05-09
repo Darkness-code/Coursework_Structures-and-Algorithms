1383b561fe06fc96eaf8bb7ecefc2cb90a12f80d
Darkness-code <79974383+Darkness-code@users.noreply.github.com>
2021-05-10 01:47:59 +0300

    Add files via upload

---
+++ CourseWork_Step 3/Pet.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Pet {
+
+    SimpleLinkedList products;
+
+    public Pet() {
+        this.products = new SimpleLinkedList();
+    }
+
+    public SimpleLinkedList getProducts() {
+        return products;
+    }
+}
